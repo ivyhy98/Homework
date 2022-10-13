fe3e8216f0d82647e2204356bacca7826a627a89
ivyhy98 <ivanahy98@gmail.com>
2022-10-12 22:04:15 -0400

    created timmy's story

--- w3d1hw.js
+++ w3d1hw.js
@@ -38,23 +38,59 @@ class Person {
     greet(){
         console.log(`Hey I'm ${this.name}`);
     }
-    eat(){
-        this.weight += 1;
-        this.mood += 1;
+    eat(amount){
+        this.weight += amount || 1;
+        this.mood += amount || 1;
     }
-    exercise(){
-        this.weight -= 1;
+    exercise(amount){
+        this.weight -= amount || 1;
     }
-    ageUp(){
-        this.age += 1;
-        this.height += 1;
-        this.weight += 1;
-        this.mood -= 1;
-        this.bankAccount += 10;
+    ageUp(num){
+        this.age += num || 1;
+        this.height += num || 1;
+        this.weight += num || 1;
+        this.mood -= num || 1;
+        this.bankAccount += num || 10;
     }
     buyHamster(hamster){
         this.hamsters.push(hamster);
         this.mood += 10;
+        console.log(`${hamster.getPrice()}`)
         this.bankAccount -= hamster.getPrice();
     }
 }
+//1. Instantiate a new Person named Timmy
+const timmy = new Person('Timmy');
+
+//2. Age Timmy five years
+timmy.ageUp(5);
+
+//3. At this point Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times.
+for(let i = 0; i<5; i++){
+    timmy.eat();
+}
+
+//4. Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to look good. Have him exercise five times
+for(let i = 0; i<5; i++){
+    timmy.exercise();
+}
+
+//5. Age Timmy 9 years
+timmy.ageUp(9);
+
+//6. && 7. Create a hamster named "Gus" & Set Gus's owner to the string "Timmy"
+const gus = new Hamster('Timmy', 'Gus', 15);
+
+//8. Have Timmy "buy" Gus
+timmy.buyHamster(gus);
+
+//9. Age Timmy 15 years
+timmy.ageUp(15);
+
+//10. Have Timmy eat twice
+timmy.eat(2);
+
+//11. Have Timmy exercise twice
+timmy.exercise(2)
+
+console.log(timmy);
