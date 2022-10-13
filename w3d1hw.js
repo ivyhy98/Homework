48f6b3532428eacadb50ff4a485c05e1c1f9b656
ivyhy98 <ivanahy98@gmail.com>
2022-10-12 21:47:27 -0400

    created person class

--- w3d1hw.js
+++ w3d1hw.js
@@ -14,3 +14,47 @@ class Hamster {
         return this.price;
     }
 }
+
+class Person {
+    constructor(name){
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
+
+    getName(){
+        return this.name;
+    }
+    getAge(){
+        return this.age
+    }
+    getWeight(){
+        return this.weight;
+    }
+    greet(){
+        console.log(`Hey I'm ${this.name}`);
+    }
+    eat(){
+        this.weight += 1;
+        this.mood += 1;
+    }
+    exercise(){
+        this.weight -= 1;
+    }
+    ageUp(){
+        this.age += 1;
+        this.height += 1;
+        this.weight += 1;
+        this.mood -= 1;
+        this.bankAccount += 10;
+    }
+    buyHamster(hamster){
+        this.hamsters.push(hamster);
+        this.mood += 10;
+        this.bankAccount -= hamster.getPrice();
+    }
+}
