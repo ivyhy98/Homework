ae47018a0bfa14406c05e06feca3e5a5d123d9ce
ivyhy98 <ivanahy98@gmail.com>
2022-10-21 14:23:28 -0400

    finished DOM Manipulation

--- dom-manipulation/manipulate-the dom2/script.js
+++ dom-manipulation/manipulate-the dom2/script.js
@@ -6,3 +6,81 @@ let quotes = [
   `I don't have friends. I have family.`,
   `It don't matter if you win by an inch or a mile. Winning's winning.`
 ];
+
+//Part 1
+const mainTitleEl = document.querySelector('#main-title');
+mainTitleEl.textContent = "Hi, I'm Dom. Family > Everything";
+
+//Part 2
+const bodyEl = document.querySelector('body');
+bodyEl.style.backgroundColor = '#b44010';
+
+//Part 3
+const favoriteThingsEl = document.querySelector('#favorite-things');
+favoriteThingsEl.removeChild(favoriteThingsEl.lastElementChild);
+
+//Part 4
+const specialEl = document.querySelectorAll('.special-title');
+
+specialEl.forEach((element)=>{
+  element.style.fontSize = '2rem';
+})
+
+//Part 5
+const pastEl = document.querySelector('#past-races');
+pastEl.removeChild(pastEl.lastElementChild);
+
+//===== CREATING NEW DOM ELEMENTS =====
+//Part 6
+const newLi = document.createElement('li');
+newLi.textContent = 'AZERBAIJAN';
+pastEl.append(newLi);
+
+//Part 7
+const newBlog = document.createElement('div');
+newBlog.classList.add('blog-post');
+newBlog.classList.add('purple');
+
+const mainEl = document.querySelector('.main');
+mainEl.append(newBlog);
+
+const headerBlog = document.createElement('h1');
+headerBlog.textContent = 'AZERBAIJAN';
+newBlog.append(headerBlog);
+
+const pBlog = document.createElement('p');
+pBlog.textContent = 'I COMPLETED A TOP SECRET MISSION HERE!';
+newBlog.append(pBlog);
+
+//===== EVENT HANDLERS =====
+//Part 8
+const randomQuote = function() {
+  document.querySelector('#quote-of-the-day').innerText = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+};
+
+const quoteT = document.querySelector('#quote-title');
+
+quoteT.addEventListener('click', randomQuote);
+
+//Part 8
+const blogPost = document.querySelectorAll('.blog-post');
+blogPost.forEach((element)=>{
+  element.addEventListener('mouseenter', (evt)=>{
+      evt.preventDefault();
+      if(evt.target.classList.contains('purple')){
+        evt.target.classList.remove('purple');
+        evt.target.classList.add('red');
+      }
+      console.log('mouse in')
+  })
+
+  element.addEventListener('mouseout', (evt)=>{
+      evt.preventDefault();
+      if(evt.target.classList.contains('red')){
+        evt.target.classList.remove('red');
+        evt.target.classList.add('purple');
+      }
+  })
+
+})
+
